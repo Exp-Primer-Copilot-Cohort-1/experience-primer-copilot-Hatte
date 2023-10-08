2e7ce551704113f91b40a19a0f0db94927a97358
Johan Johansson <hattese@gmail.com>
2023-10-08 17:57:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+    var memberSkills = document.getElementById("memberSkills");
+    var skillsMember = document.getElementById("skillsMember"); 
+}
